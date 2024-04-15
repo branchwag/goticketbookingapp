af714d5a3b6168f2e81c3969d2b7194f51c6063c
LeAnne B <branchleannelmb@gmail.com>
2024-04-15 19:01:05 -0400

    adds in work done so far

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking app!\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still avaliable.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+
+
+}
