09bd693f41a16290be045005d9228e76dfad3ae1
LeAnne B <branchleannelmb@gmail.com>
2024-04-15 19:25:56 -0400

    allows for user input

---
+++ README.md
@@ -0,0 +1,5 @@
+# A Ticket Booker
+
+A simple console ticket booking app using GO, made following Nana's lovely instruction here:
+</br>
+https://www.youtube.com/watch?v=yyUHQIec83I&ab_channel=TechWorldwithNana
--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("Welcome to %v booking app!\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still avaliable.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var userName string
+	var userTickets int
+	//ask user for name
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&userName)
 
-
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
